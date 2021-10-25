98ffc323e8be47c708c50f0b0848e482ecb1a03d
Ville <koivisto.vt@gmail.com>
2021-10-25 13:15:58 +0300

    added project for finding primes

---
+++ erastothenes/erastothenes.py
@@ -0,0 +1,12 @@
+"""
+List prime numbers to the number given by the user.
+"""
+
+def find_primes(number):
+    pass
+
+
+if __name__ == "__main__":
+    limiting_number = int(input("Give a number: "))
+
+    find_primes(limiting_number)
