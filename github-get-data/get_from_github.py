bfb4fc7036ed7a5fedef4b746770fac04dbf7dc5
Ville <koivisto.vt@gmail.com>
2021-10-25 12:39:44 +0300

    version 1

--- github-get-data/get_from_github.py
+++ github-get-data/get_from_github.py
@@ -2,6 +2,39 @@
 Simple script for getting all Python-repos from Github API
 """
 
+import urllib.request
+import json
+
+def get_github_data():
+    remote_url = "https://api.github.com/search/repositories?q=language:python"
+
+    with urllib.request.urlopen(remote_url) as response:
+        github_data = response.read()
+        github_json = json.loads(github_data)
+
+    return github_json
+
 
 if __name__ == "__main__":
-    pass
+    print("All Python related repos in Github atm:")
+    python_thingies = get_github_data()
+
+    get_forks = []
+
+    for data_items in python_thingies["items"]:
+        json_data = []
+
+        id = data_items["id"]
+        name = data_items["name"]
+        fork_count = data_items["forks_count"]
+        
+        json_data.append(fork_count)
+        json_data.append(id)
+        json_data.append(name)
+
+        get_forks.append(json_data)
+
+    sorted_forks = sorted(get_forks, reverse=True)
+
+    for item in sorted_forks:
+        print(f"{item[0]}.{item[2]}.{item[1]}")
