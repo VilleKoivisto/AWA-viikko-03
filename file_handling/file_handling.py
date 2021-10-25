bc414187c8ecd3ac71c5e2b6c6c59eaebd15e291
Ville <koivisto.vt@gmail.com>
2021-10-25 13:07:03 +0300

    version 1 for basic file handling miniproj

--- file_handling/file_handling.py
+++ file_handling/file_handling.py
@@ -1,3 +1,22 @@
 """
-
+Järjestää tiedostosta luetut sanat ensisijaisesti pituuden mukaan ja toissijaisesti aakkosittain.
+Kirjoittaa järkätyt sanat uuteen tiedostoon
 """
+
+def read_the_file():
+    with open("alkusanat.txt", "r") as the_file:
+        words = []
+
+        for word in the_file:
+            words.append(word.rstrip("\n"))
+
+    words.sort()
+    words.sort(key=len)
+
+    with open("loppusanat.txt", "w") as another_file:
+        for word in words:
+            another_file.write(f"{word}\n")
+
+
+if __name__ == "__main__":
+    read_the_file()
