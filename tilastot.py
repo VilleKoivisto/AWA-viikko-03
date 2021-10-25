11d06f4aeeed9ed11b7a687b3ea6cb9c886b7f77
Ville <koivisto.vt@gmail.com>
2021-10-25 11:01:45 +0300

    first version of tilastot.py

---
+++ tilastot.py
@@ -0,0 +1,64 @@
+"""
+Testing Python Math-module, gets from user input:
+- min, max, mean, median, mode
+
+User input in format: num, num, num, num, etc
+"""
+
+import statistics
+
+
+def get_values(user_input):
+    values_in_temp_list = user_input.split(",") # temp list for str -> list comversion
+
+    values_in_list = [] # new list init for stripping
+        
+    try:
+        for value in values_in_temp_list:
+            value = float(value.strip())
+
+            values_in_list.append(value)
+
+    except ValueError:
+        raise ValueError
+
+    v_min = min(values_in_list)
+    v_max = max(values_in_list)
+    v_mean = statistics.mean(values_in_list)
+    v_median = find_median(values_in_list)
+    v_mode = statistics.mode(values_in_list)
+
+    return v_min, v_max, v_mean, v_median, v_mode
+
+
+def find_median(values_list):
+    count = len(values_list)
+    index = count // 2
+
+    if count % 2:
+        return sorted(values_list)[index]
+    
+    return sum(sorted(values_list)[index - 1: index + 1]) / 2
+
+
+if __name__ == "__main__":
+    while True:
+        print("Give numbers in format num,num,num,num,etc.")
+        print("Empty line will terminate the script.")
+        user_input = input(":")
+
+        if user_input == "":
+            break
+
+        try:
+            get_values(user_input)
+            num_min, num_max, num_mean, num_median, num_mode = get_values(user_input)
+
+        except ValueError:
+            print("\nPlease give only integers or float values.\n")
+
+        print(f"\nMin value: {num_min}")
+        print(f"Max value: {num_max}")
+        print(f"Mean value: {num_mean}")
+        print(f"Median: {num_median}")
+        print(f"Mode: {num_mode}\n")
