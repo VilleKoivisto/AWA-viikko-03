4525aa7a44c10e003070671db22faf9272b8192a
Ville <koivisto.vt@gmail.com>
2021-10-26 09:50:21 +0300

    started gron job project

---
+++ gron-job/gronjob.py
@@ -0,0 +1,17 @@
+"""
+Yksinkertainen GRON job
+"""
+
+import os
+
+
+def do_the_job():
+    try:
+        with open("timestamp.txt") as file_open:
+            pass
+
+    except OSError:
+        pass
+
+if __name__ == "__main__":
+    pass
